426be04300f04ad78d416540a8efafbcba84469b
Chunghav <[trychunghav168@gmail.com]>
2021-04-18 16:56:44 +0700

    new commit1

---
+++ js/sort.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+    $("#sortByRank").click(function() {
+
+        $(".uniDrop-links li").sort(function(a, b) {
+          return parseInt(a.id) - parseInt(b.id);
+        }).each(function() {
+          var elem = $(this);
+          elem.remove();
+          $(elem).appendTo(".uniDrop-links");
+        });
+        $("#currentSort").text("(sorted by ranking)");
+    });
+  });
+
+
+  $(document).ready(function(){
+      $("#sortByAlpha").click(function(){
+
+        $(".uniDrop-links li").sort(function(a, b){
+          return $(a).text().localeCompare($(b).text());
+      }).appendTo(".uniDrop-links");
+      $("#currentSort").text("(sorted by alphabet)");
+      });
+  });
+
+
+  //////////toggle sort//////////
+
+
+  $(document).ready(function() {
+    $("#sortByRank-btn-toggle").click(function() {
+  
+      if ($(".rank-sort2").hasClass('fa-sort-numeric-down')) {
+        $(".uniDrop-links-toggle li").sort(function(a, b) {
+          return parseInt(b.id) - parseInt(a.id);
+        }).each(function() {
+          var elem = $(this);
+          elem.remove();
+          $(elem).appendTo(".uniDrop-links-toggle");
+        });
+        $(".rank-sort2").toggleClass('fa-sort-numeric-up fa-sort-numeric-down');
+      } 
+      else {
+        $(".uniDrop-links-toggle li").sort(function(a, b) {
+          return parseInt(a.id) - parseInt(b.id);
+        }).each(function() {
+          var elem = $(this);
+          elem.remove();
+          $(elem).appendTo(".uniDrop-links-toggle");
+        });
+        $(".rank-sort2").toggleClass('fa-sort-numeric-up fa-sort-numeric-down');
+  
+      }
+  
+    });
+  });
+
+
+  $(document).ready(function(){
+      $("#sortByAlpha-btn-toggle").click(function(){
+
+        if($(".alpha-sort2").hasClass('fa-sort-alpha-down')){
+            $(".uniDrop-links-toggle li").sort(function(a, b){
+                return $(b).text().localeCompare($(a).text());
+            }).appendTo(".uniDrop-links-toggle");
+            $(".alpha-sort2").toggleClass('fa-sort-alpha-up fa-sort-alpha-down');
+        }
+
+        else{
+            $(".uniDrop-links-toggle li").sort(function(a, b){
+            return $(a).text().localeCompare($(b).text());
+        }).appendTo(".uniDrop-links-toggle");
+        $(".alpha-sort2").toggleClass('fa-sort-alpha-up fa-sort-alpha-down');
+        }
+
+        
+      })
+  })
+
+  //////////////////////////////////////////////////////
+
+
+  $(".uni-major-list li").sort(function(a, b){
+    return $(a).text().localeCompare($(b).text());
+}).appendTo(".uni-major-list");
---
+++ pages/New_University_Scraper/newscript.js
@@ -0,0 +1,94 @@
+function checkValidation(element, error) { 
+    const errorClass = 'text-danger d-none';
+    const errorClassVisible = 'text-danger';
+
+    const errorName = document.getElementById(error);
+    if(document.getElementById(element).value === '') {
+        errorName.className = errorClassVisible;
+    } else {
+        errorName.className = errorClass;
+    }
+}
+
+
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyDp6wss0FuZO54he8yRl9ST2XpuiR_BS9A",
+    authDomain: "webdb-f8c0c.firebaseapp.com",
+    projectId: "webdb-f8c0c",
+    storageBucket: "webdb-f8c0c.appspot.com",
+    messagingSenderId: "799530796204",
+    appId: "1:799530796204:web:c4b4488952ccb517794b09",
+    measurementId: "G-9SHHN3PHRC"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  function register(event) {
+     //Prevent Default Form Submission Behavior
+     event.preventDefault()
+
+     checkValidation('inputemail','erroremail');
+     checkValidation('inputName','errorName');
+     checkValidation('inputPhone','errorphone');
+     checkValidation('inputpw','errorpw');
+     checkValidation('inputpwre','errorpwre');
+
+    var email = document.getElementById('inputemail').value;
+    var userName = document.getElementById('inputName').value;
+    var phone = document.getElementById('inputPhone').value;
+    var password = document.getElementById('inputpwre').value;
+   firebase.auth().createUserWithEmailAndPassword(email,password).then(function(){
+    // alert('User Register successfully');
+    
+    var id=firebase.auth().currentUser.uid;
+    firebase.database().ref('Users/'+id).set({
+        Email: email,
+        UserName: userName,
+        PhoneNumber: phone,
+        Password: password,
+
+
+    });
+    console.log("Data saved")
+    //alert
+ alert("Your Form Has Been Submitted Successfully")
+
+
+   }).catch(function(error){
+       //alert
+    alert("Please input in textbox!!")
+    var errorcode=error.code;
+    var errormsg=error.message;
+
+   });
+ 
+
+  }//end register
+
+  function login(event){
+      //Prevent Default Form Submission Behavior
+     event.preventDefault()
+
+     checkValidation('inputemail','erroremail');
+     checkValidation('inputpwre','errorpwre');
+
+     var email = document.getElementById('inputemail').value;
+     var password = document.getElementById('inputpwre').value;
+    
+    firebase.auth().signInWithEmailAndPassword(email,password).then(function(){
+ 
+     var id=firebase.auth().currentUser.uid;
+     location.replace("../quiz.html");
+     localStorage.setItem('id',id);
+     console.log("Data true")
+      alert("Your Form Has Been log in Successfully")
+      
+    }).catch(function(error){
+ alert("Your email or password are wrong please try again")
+     var errorCode=error.code;
+     var errorMsg=error.message;
+ 
+    });
+   }
